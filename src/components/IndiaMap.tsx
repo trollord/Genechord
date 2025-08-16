a860f4ea15461d65bc937d0c7d7df5a4cfd4ea5f
sidmeshram1003 <sidmeshram1003@gmail.com>
2025-08-16 16:04:47 +0530

    fix: map css and legend

--- src/components/IndiaMap.tsx
+++ src/components/IndiaMap.tsx
@@ -5,12 +5,15 @@ interface IndiaMapProps {
   className?: string;
 }
 
-const IndiaMap: React.FC<IndiaMapProps> = ({ operatingStates, className = "" }) => {
+const IndiaMap: React.FC<IndiaMapProps> = ({
+  operatingStates,
+  className = "",
+}) => {
   return (
     <div className={`w-full flex justify-center items-center ${className}`}>
       <div className="w-full max-w-md mx-auto">
-        <img 
-          src="/india.svg" 
+        <img
+          src="/india_modified.svg"
           alt="Map of India showing operating states"
           className="w-full h-auto"
         />
